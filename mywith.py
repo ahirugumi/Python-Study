fce89c352c1b448ffcbb8f1d51550e94b53b3197
ahirugumi <puts.helloworld@nifty.com>
2014-07-05 18:03:43 +0900

    コメント追加

--- mywith.py
+++ mywith.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# 通常のやり方
 def mynotwith():
     myfile=open('myclass.py')
     try:
@@ -8,11 +9,14 @@ def mynotwith():
     finally:
         myfile.close()
 
+# withのやり方
 def mywith():
     with open('myclass.py') as myfile:
         for line in myfile:
             print line,
 
+# with独自クラス
+# __enter__, __exit__を持つクラスを作成する
 class MyWithClass:
     def __init__(self, args):
         print "init"
